4490641e7f3f7a6809e3072ce99a83ca11a9a540
mkyw <michaelkwong03@gmail.com>
2023-05-27 22:10:33 -0700

    updated 5/27

--- script.js
+++ script.js
@@ -20,3 +20,15 @@ $(document).ready(function () {
         $(window).scrollTop(sessionStorage.scrollTop);
     }
 });
+
+function refreshPage() {
+    var page_y = $(document).scrollTop();
+    window.location.href = window.location.href + '?page_y=' + page_y;
+}
+window.onload = function () {
+    setTimeout(refreshPage, 35000);
+    if (window.location.href.indexOf('page_y') != -1) {
+        var match = window.location.href.split('?')[1].split("&")[0].split("=");
+        $('html, body').scrollTop(match[1]);
+    }
+}
